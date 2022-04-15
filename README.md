0b2c71b855c66444e9e60c9ef52e1761cf6d083b
mstrihovanecz <m.strihovanecz@gmail.com>
2022-04-15 16:16:02 +0200

    add colorMixingLamp project

---
+++ README.md
@@ -0,0 +1,1 @@
+Arduino hobby projects in Assembly and C.
---
+++ colorMixingLamp/colorMixingLamp.ino
@@ -0,0 +1,47 @@
+/******************************************************************************************************************************************
+*                                               AVR(-GCC) ASSEMBLY - COLOR MIXING LAMP                                                    *
+*    AUTHOR     : S.M.                                                                                                                    *
+*    CREATE DATE: 2022.04.12.                                                                                                             *
+*    MODIFY DATE: 2022.04.15.                                                                                                             *
+*******************************************************************************************************************************************/
+
+extern "C" {                         //Assembly hivashoz
+    void initColorMixingLamp(); 
+    void A0_AnalogRead();
+    void A1_AnalogRead();
+    void A2_AnalogRead();
+    
+    void RGBLEDOn(byte);
+    void printMessage(byte);
+    void delay2sec();
+}
+
+#define A0_ANALOG_PORT_ID 0
+#define A1_ANALOG_PORT_ID 1
+#define A2_ANALOG_PORT_ID 2
+
+void setup() {
+    initColorMixingLamp();           //Inicializalas: portok es serial kommunikacio beallitasa.
+}
+
+void loop() {
+    A0_AnalogRead();                 /*A0 portrol erkezo analog jel:
+                                           - Beolvasasa.
+                                           - Digitalis jelle konvertalasa.*/
+    printMessage(A0_ANALOG_PORT_ID);     //- Kuldese OUTPUT-ra: - Serial port pl.: "Red value: 020".
+    RGBLEDOn(A0_ANALOG_PORT_ID);                              //- RGBLed "red" laba.
+
+    A1_AnalogRead();                 /*A1 portrol erkezo analog jel:
+                                           - Beolvasasa.
+                                           - Digitalis jelle konvertalasa.*/
+    printMessage(A1_ANALOG_PORT_ID);     //- Kuldese OUTPUT-ra: - Serial port pl.: "Blue value: 030".
+    RGBLEDOn(A1_ANALOG_PORT_ID);                              //- RGBLed "blue" laba.
+
+    A2_AnalogRead();                 /*A2 portrol erkezo analog jel:
+                                           - Beolvasasa.
+                                           - Digitalis jelle konvertalasa.*/
+    printMessage(A2_ANALOG_PORT_ID);     //- Kuldese OUTPUT-ra: - Serial port pl.: "Green value: 005".
+    RGBLEDOn(A2_ANALOG_PORT_ID);                              //- RGBLed "green" laba.
+    
+    delay2sec();                     //Delay = 2 sec = 2.000.000 ms
+}

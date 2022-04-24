1d91ce923b95b965d7c1d0944b97c5fd128851be
mstrihovanecz <m.strihovanecz@gmail.com>
2022-04-24 19:46:22 +0200

    Added moodCue_ASM files.

---
+++ moodCue/moodCue_ASM/moodCue_ASM.ino
@@ -0,0 +1,25 @@
+extern "C" {
+    void initMoodCue_ASM();
+    void setAngle(byte);
+}
+
+#define POTENTIOMETER_PIN A0
+#define ANALOG_VALUE_MIN  0
+#define ANALOG_VALUE_MAX  1023
+#define SERVO_MIN_ANGLE   0
+#define SERVO_MAX_ANGLE   179
+
+int potentiometerValue = 0;
+byte servoAngle        = 0;
+
+void setup() {
+    initMoodCue_ASM();
+}
+
+void loop() {
+    potentiometerValue = analogRead(POTENTIOMETER_PIN);
+    servoAngle         = map(potentiometerValue, ANALOG_VALUE_MIN, ANALOG_VALUE_MAX, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE);
+
+    setAngle(servoAngle);
+    delay(20);
+}
